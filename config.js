84c89a2e06a2f9f6f894c594205ac9b1d0e0dbc9
isabellavfortunato <isavfortunato@gmail.com>
2026-09-11 08:30:35 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBhfIEy0AhqumVHWLRfIZvp4aIehn1SruU",
+  authDomain: "caderno-roteiros.firebaseapp.com",
+  projectId: "caderno-roteiros",
+  storageBucket: "caderno-roteiros.firebasestorage.app",
+  messagingSenderId: "384263947385",
+  appId: "1:384263947385:web:514e4ee3ec379dc7c640a8"
+};
